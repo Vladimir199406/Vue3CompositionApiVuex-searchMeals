6594552990fcf813b7d6f1f5fa189b95a5e1ff30
vo <vovasso550@rambler.ru>
2023-07-12 18:43:27 +0300

    added-proto-of-meals-view

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,16 @@
 <template>
-  <pre>{{ meals }}</pre>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search for Meals"
+    />
+    <div class="flex gap-1 justify-center gap-2 mt-2">
+      <router-link :to="{name: 'byLetter', params: {letter}}" v-for="letter of letters" :key="letter">
+        {{ letter }}
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -7,4 +18,7 @@ import { computed } from "vue";
 import store from "@/store/index";
 
 const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+
+
 </script>
