a6f3d45717c5b231875ddb26242a9e951e8cc265
vo <vovasso550@rambler.ru>
2023-07-16 19:34:05 +0300

    primary-meals-list-and-card-ui

--- src/views/MealsByName.vue
+++ src/views/MealsByName.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="p-6">
+  <div class="p-8 pb-0">
     <input
       v-model="keyword"
       type="text"
@@ -7,20 +7,54 @@
       placeholder="Search for Meals"
       @change="searchMeals"
     />
-    <pre>
-      {{ meals }}
-    </pre>
+    <div class="grid grid-cols-1 md:grid-cols-3 gap-5 p-8">
+      <div
+        v-for="meal of meals"
+        :key="meal.idMeal"
+        class="bg-white shadow rounded-xl"
+      >
+        <router-link :to="{name: 'mealDetails', params: {id: meal.idMeal}}">
+          <img
+            :src="meal.strMealThumb"
+            alt="strMeal"
+            class="rounded-t-xl w-full h-48 object-cover"
+          />
+        </router-link>
+        <div class="px-3">
+          <h3 class="py-3 font-bold">{{ meal.strMeal }}</h3>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit.</p>
+          <div class="py-3 flex items-center justify-between">
+            <a
+              :href="meal.strYotube"
+              target="_blank"
+              class="px-3 py-2 rounded border-2 border-red-600 bg-red-500 text-white hover:bg-red-600 hover:text-white hover:cursor-pointer transition-colors"
+              >Yotube</a
+            >
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { computed, ref, watch } from 'vue'
-import store from '../store'
+import { computed, onMounted, ref } from "vue";
+import store from "../store";
+import { useRoute } from "vue-router";
 
-const keyword = ref('')
-const meals = computed(() => store.state.searchedMeals)
+const route = useRoute()
+
+const keyword = ref("");
+const meals = computed(() => store.state.searchedMeals);
 
 function searchMeals() {
-  store.dispatch('searchMeals', keyword.value)
+  store.dispatch("searchMeals", keyword.value);
 }
+
+onMounted(() => {
+  keyword.value = route.params.name
+  if (keyword.value) {
+    searchMeals()
+  }
+})
 </script>
